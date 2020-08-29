ee4a5206fb2b9699cb5504a266f2009c869d19ab
Ashley Hunt <ashhunt07@gmail.com>
2020-08-29 01:57:45 -0500

    attempting to add additional functionality, and updated CSS style

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 
+$(document).ready(function(){
 //global variables
 // let city = [];
 
-var cont = $("#container").attr('class', 'content row col-12');
+var cont = $("#container").attr('class', 'content row col-12 container-fluid');
 
 //left area containers
-var lookUp = $("<div>").attr('class', 'lookUp col-4');
+var lookUp = $("<div>").attr('class', 'lookUp col-8');
 
 var searchDiv = $("<textarea>").attr('class', 'inputCity');
 
@@ -16,44 +17,75 @@ var searchHistory = $("<p>").attr('class', 'seachList');
 
 
 //right area containers
-var searchResults = $("<div>").attr('class', 'searchResult row col-8');
+var searchResults = $("<div>").attr('class', 'searchResult row col-12 container-fluid d-flex flex-row flex-wrap align-items-center justify-content-center');
 
-var currentCity = $("<div>").attr('class', 'currentCity card-body row col-12');
-var currentTitle = $("<h3>").attr('class', 'card-title');
-var cityTemp = $("<p>").attr('class', 'card-text');
-
-var humidity = $("<p>").attr('class', 'card-text');
-var windSpeed = $("<p>").attr('class', 'card-text');
-var uv = $("<p>").attr('class', 'card-text');
-
+//append lookUp seach elements
+$("#header").append(lookUp);
+lookUp.append(searchDiv);
+lookUp.append(buttonEl);
+    buttonEl.append(searchEl);
+lookUp.append(searchHistory);
 
 
-// let city = searchDiv.val();
 
-let city = "Austin";
 const apiKey = "67d19e2b34aa4341617b42310a8a49b4";
 let currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 let dailyURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&exclude=hourly,minutely&appid=" + apiKey;
+
+
 
 //pull from local storage
 // let value = localStorage.getItem(lastCitySearch);
 // searchHistory.val(value);
 
-$(document).ready(function(){
-    
-    
-// buttonEl.on("click", function() {
-//     // var city = $(this).siblings("input").val();
-//     event.preventDefault();  
-//     searchDiv.val("");
-    
-//     getWeather(city);
-// function getWeather(city){
+
+
+
+var city = searchDiv.val();
+searchDiv.val("");
+
 
     
-        // windSpeed.text(response.wind.speed);
-        // console.log(response.wind.speed);
     
+    buttonEl.on("click", function(){
+        event.preventDefault();    
+
+        weather(city);
+        
+    });
+    
+    //this function will display current info
+    function locationInfo(currentCity, cityTemp, humidity, windSpeed, currentTitle){
+
+        // $("#location").empty();
+        // $("#temp").empty();
+        // $("#humidity").empty();
+        // $("#wind").empty();
+
+        var currentCity = $("<div>").attr('class', 'currentCity card-body row col-12');
+        var currentTitle = $("<h3>").attr('class', 'card-title');
+        var cityTemp = $("<p>").attr('class', 'card-text');
+
+        var humidity = $("<p>").attr('class', 'card-text');
+        var windSpeed = $("<p>").attr('class', 'card-text');
+    
+    
+        
+        //append results
+        cont.append(searchResults);
+
+        //append currentCity 
+        searchResults.append(currentCity);
+
+        //current city top level info
+        currentCity.append(currentTitle)
+        currentCity.append(cityTemp);
+        currentCity.append(humidity);
+        currentCity.append(windSpeed);
+
+    }
+    
+
     
     $.ajax({
         url: dailyURL,
@@ -75,13 +107,13 @@ $(document).ready(function(){
 
         for(i = 0; i < 5; i++){  
             
-            currentCity.text("");
-            currentCity.text(weatherResponse.name);
+            // currentCity.text("");
+            // currentCity.text(weatherResponse.name);
 
             //results
-            var weatherEl = $("<div>").attr('class', 'weatherBlock card-body align-items-start flex-wrap');
+            var weatherEl = $("<div>").attr('class', 'weatherBlock card-body');
             var weatherDate = $("<h6>").attr('class', 'card-title');
-            var weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + iconCode + "@2x.png");
+            var weatherIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + iconCode + "@2x.png");
                 var iconCode = weatherResponse.weather[0].icon;
             var weatherTemp = $("<p>").attr('class', 'card-text');
             var weatherHumid = $("<p>").attr('class', 'card-text');
@@ -98,17 +130,18 @@ $(document).ready(function(){
 
             //result or search weather block
             searchResults.append(weatherEl);
-
-            //append weather block elements
-            weatherEl.append(weatherDate);
-            weatherEl.append(weatherIcon);
-            weatherEl.append(weatherTemp);
-            weatherEl.append(weatherHumid);
+                //append weather block elements
+                weatherEl.append(weatherDate);
+                weatherEl.append(weatherIcon);
+                weatherEl.append(weatherTemp);
+                weatherEl.append(weatherHumid);
 
         console.log(weatherResponse.name);
         console.log(weatherResponse.weather[0].icon);
         console.log(weatherResponse.main.humidity);
         console.log(weatherResponse.main.temp); 
+
+        uvIndex(api, latitude, longitude);
 
         }
 
@@ -119,38 +152,32 @@ $(document).ready(function(){
         })   
         
     
-
-//append lookUp seach elements
-cont.append(lookUp);
-lookUp.append(searchDiv);
-lookUp.append(buttonEl);
-    buttonEl.append(searchEl);
-lookUp.append(searchHistory);
-
-
-//append results
-cont.append(searchResults);
-
-//append currentCity 
-searchResults.append(currentCity);
-
-//current city top level info
-currentCity.append(currentTitle)
-currentCity.append(cityTemp);
-currentCity.append(humidity);
-currentCity.append(windSpeed);
-currentCity.append(uv);
+        function uvIndex(apiKey, lat, lon){
+            var uvIndexURL= "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey +"&lat=" + lat + "&lon=" + lon;
+            
+    $.ajax({
+            url: uvIndexURL,
+            method: "GET"
+    }).then(function(response) {
+            
+            uv.empty();
+            
+        
+            var uv = $("<p>").attr('class', 'card-text').text("UV Index: " + response.value);
+            currentCity.append(uv);
+            
+            
+            
+            });
+    }
 
 
 
-    
 
 
 
-// })
-    // }
 
-    // getWeather()
+    locationInfo()
 
 })
 });
