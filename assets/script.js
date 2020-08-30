9db5777fe75b5463b9b08fb5ed08fafc51d8073a
Ashley Hunt <ashhunt07@gmail.com>
2020-08-29 21:58:47 -0500

    testing empty functions

--- assets/script.js
+++ assets/script.js
@@ -32,6 +32,7 @@ const apiKey = "67d19e2b34aa4341617b42310a8a49b4";
                 $(searchDiv).val();
 
                 $("#container").empty();
+                $(".inputCity").empty();
                 weather(city);
             })
 
